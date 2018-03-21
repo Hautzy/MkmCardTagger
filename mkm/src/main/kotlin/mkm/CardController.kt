13e6969c715cc8de4fe949dd08ed445c87785dc8
Julian Hautzmayer <hautzy72@gmail.com>
2018-03-21 18:48:37 +0100

    Add filter function and test data.

--- mkm/src/main/kotlin/mkm/CardController.kt
+++ mkm/src/main/kotlin/mkm/CardController.kt
@@ -3,6 +3,7 @@ package mkm
 import org.jsoup.Jsoup
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.io.File
 import java.io.IOException
@@ -14,17 +15,60 @@ class CardController(private val cardRepository: CardRepository) {
     val LIST_URL: String = "https://www.cardmarket.com/de/Magic/Products/Singles?name=&idExpansion=0&idRarity=&onlyAvailable=&sortBy=popularity&sortDir=desc&view=list&resultsPage="
     val ROOT_IMG_URL: String = "https://www.cardmarket.com/de/Magic"
 
+    @GetMapping("/saveTestData")
+    fun initTestData(): List<Card> {
+        var cardTestDataList = listOf(
+                Card(0,
+                        "Field of Ruin",
+                        "Schauplatz der Verheerung",
+                        "/de/Magic/Products/Singles/Ixalan/Schauplatz+der+Verheerung",
+                        "/img/9e848a75683f5382fed04513a016340b/expansionicons/expicons.png"),
+                Card(1,
+                        "Unclaimed Territory",
+                        "Unbeanspruchtes Territorium",
+                        "/de/Magic/Products/Singles/Ixalan/Unbeanspruchtes+Territorium",
+                        "/img/9e848a75683f5382fed04513a016340b/expansionicons/expicons.png"),
+                Card(2,
+                        "Island",
+                        "Insel",
+                        "/de/Magic/Products/Singles/Unstable/Insel",
+                        "/img/9e848a75683f5382fed04513a016340b/expansionicons/expicons.png"))
+
+        cardRepository.saveAll(cardTestDataList)
+        return cardTestDataList
+    }
+
+    @GetMapping("/deleteTestData")
+    fun removeTestData(): String {
+        cardRepository.deleteAll()
+        return "done"
+    }
+
     @GetMapping("/findAll")
-    fun findAll(): List<Card> = cardRepository.findAll()
+    fun findAll(): List<Card> = cardRepository.findAll().toList()
+
+    @GetMapping("/findByEnglishName")
+    fun findByEnglishName(@RequestParam(value = "englishName", defaultValue = "") englishNameParam: String,
+                          @RequestParam(value = "resultCnt", defaultValue = "10") resultCntParam: String): List<Card> {
+        var cnt: Int? = resultCntParam.toIntOrNull()
+        cnt = if (cnt == null || cnt < 1) 10 else cnt
+        if (englishNameParam.isNullOrEmpty())
+            return cardRepository.findAll().toList().subList(0, cnt)
+        else
+            return cardRepository.findByEnglishName(englishNameParam).subList(0, cnt)
+    }
+
+    @GetMapping("/cardCnt")
+    fun cardCnt(): Int = countOfCards()
 
     @GetMapping("/loadToCsv")
     fun loadToCsv(): Long {
+        var maxCardCnt = countOfCards()
         var cardCnt = 0L
         var site = 0
-        var run = true
         try {
             File("cards.csv").printWriter().use { out ->
-                while (run) {
+                while (cardCnt < maxCardCnt) {
                     try {
                         val doc = Jsoup.connect(LIST_URL + site).get()
                         val tables = doc.select("table")
@@ -44,16 +88,12 @@ class CardController(private val cardRepository: CardRepository) {
 
                                 var c = Card(cardCnt, englishName, germanName, detailUrl, imgUrl)
                                 out.println("${c.id};${c.englishName};${c.gerName};${c.detailUrl};${c.imgUrl}")
-
                                 cardCnt++
                                 println(cardCnt.toString() + " | " + englishName)
                             }
                         }
                         site++
                         println("---------> currently on site: " + site)
-                        if (cardCnt > 41984) {
-                            run = false
-                        }
                     } catch (e: IOException) {
                         e.printStackTrace()
                     }
@@ -67,6 +107,9 @@ class CardController(private val cardRepository: CardRepository) {
     }
 
     fun countOfCards(): Int{
-        return 0
+        val doc = Jsoup.connect(LIST_URL + 0).get()
+        var cntElement = doc.getElementById("hitCountBottom")
+        var cntOfCards = cntElement.html().toInt()
+        return cntOfCards
     }
 }
--- mkm/src/main/kotlin/mkm/CardRepository.kt
+++ mkm/src/main/kotlin/mkm/CardRepository.kt
@@ -1,7 +1,12 @@
 package mkm
 
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CardRepository: JpaRepository<Card, Long>
+interface CardRepository: CrudRepository<Card, Int> {
+    @Query("SELECT c FROM Card c WHERE UPPER(c.englishName) LIKE '%' || UPPER(:englishName) || '%'")
+    fun findByEnglishName(@Param("englishName") englishName: String): List<Card>
+}
